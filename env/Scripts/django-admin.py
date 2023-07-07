b5be7a13d9535cd769761e8841260344a1486f67
Rohit <rohit80025@gmail.com>
2023-07-07 23:08:39 +0530

    this is my first commet

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Rohit\Project\Django_project\Carzone_Project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
