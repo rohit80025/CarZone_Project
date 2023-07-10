6e453fc875febe6701187f5521756c65d7e6c9a6
Rohit <rohit80025@gmail.com>
2023-07-11 00:15:47 +0530

    commit-4

--- carzone/settings.py
+++ carzone/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Rohit@123',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -117,3 +121,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/apps.py
+++ pages/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class PagesConfig(AppConfig):
     name = 'pages'
+
+
+class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'Myapp'
